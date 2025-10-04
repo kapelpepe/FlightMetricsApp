d5421e88a9f64403f027e342eaf3e4a9af398e53
Kacper <kacper.gwiazda7@gmail.com>
2025-10-04 15:44:40 +0200

    Initial Commit

---
+++ FlightMetricsApp/FlightMetricsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightMetricsAppApp.swift
+//  FlightMetricsApp
+//
+//  Created by Kacper Gwiazda on 04/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlightMetricsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
