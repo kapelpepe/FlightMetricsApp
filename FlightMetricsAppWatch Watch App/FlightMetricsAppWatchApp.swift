49140e5f316667662c86abf559f47d1ca07ead96
Kacper <kacper.gwiazda7@gmail.com>
2025-10-04 17:57:40 +0200

    Add initial Apple Watch target setup

---
+++ FlightMetricsAppWatch Watch App/FlightMetricsAppWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightMetricsAppWatchApp.swift
+//  FlightMetricsAppWatch Watch App
+//
+//  Created by Kacper Gwiazda on 04/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlightMetricsAppWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
