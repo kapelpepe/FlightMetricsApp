4655851ba066d732453471fa0c76ede8db726e15
Gwiazda <gwiazdak@RLBWMOC0724238.local>
2025-11-24 00:32:29 +0100

    Updated app icons

--- FlightMetricsApp/Views/HomeView.swift
+++ FlightMetricsApp/Views/HomeView.swift
@@ -31,6 +31,7 @@ struct HomeView: View {
                         HStack {
                             HStack { // header
                                 Image(systemName: "airplane.circle.fill")
+                                //Image("AppIconLogo")
                                     .resizable()
                                     .frame(width: 44, height: 44)
                                     .foregroundColor(.appFirstAccent)
